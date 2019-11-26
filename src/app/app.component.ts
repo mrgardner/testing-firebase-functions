59a717c7cbc76afcfd160412973db5afbb883689
Matthew Gardner <mgardner@kenzan.com>
2019-11-26 09:51:37 -0500

    Saving work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'testing-firebase-functions';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('https://us-central1-personal-site-be426.cloudfunctions.net/helloWorld').subscribe(res => console.log(res));
+  }
 }
